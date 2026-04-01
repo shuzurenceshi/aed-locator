1bc3d4dfb478422ffa0e90fbaaddb307eff9dcc0
shuzurenceshi <shuzurenceshi@outlook.com>
2026-04-01 09:12:21 +0800

    auto sync: 2026-04-01 09:12:21

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  // 关闭静态导出（使用 static渲染）
-  experimental: {
-    serverActions: true,
-    serverComponents: 'inline' as true,
-  },
+  output: 'export',
 };
 
 export default nextConfig;
